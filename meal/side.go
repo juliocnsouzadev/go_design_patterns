d6cb5a22efedc2c5c0bede9415e8faf39b353da3
juliocnsouzadev <juliocnsouzadev@gmail.com>
2022-06-18 12:52:56 +0100

    add builder pattern implementation demo

---
+++ meal/side.go
@@ -0,0 +1,31 @@
+package meal
+
+import "strings"
+
+type Side struct {
+	name        string
+	description string
+}
+
+func (s *Side) String() string {
+	var sb strings.Builder
+	sb.WriteString(s.name)
+	sb.WriteString(" (")
+	sb.WriteString(s.description)
+	sb.WriteString(")")
+	return sb.String()
+}
+
+type SideBuilder struct {
+	*Builder
+}
+
+func (b *SideBuilder) Named(name string) *SideBuilder {
+	b.meal.side.name = name
+	return b
+}
+
+func (b *SideBuilder) WithDescription(description string) *SideBuilder {
+	b.meal.side.description = description
+	return b
+}
